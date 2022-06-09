7075030bf66bfb7ee49483fb09fc1065a01f9ce3
Felipe Narvaez <felipe4k@outlook.es>
2022-06-09 16:42:42 -0500

    this is important obtain the version of library fist commit

---
+++ VersionsPandaEtc.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy  as np
+a=pd.__version__
+b=np.__version__ #it prints me the version of pandas and numpy it will work the same with others
+print('the verion of pandas is {} and te version of numpy is {}'.format(a,b)) # me imprime las versiones de pandas y numpy funcionra igual con otras 

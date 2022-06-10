89db4c350eedbc3fe61e1877666dfff828f9144d
Felipe Narvaez <felipe4k@outlook.es>
2022-06-09 20:18:12 -0500

    the converion first dt to list

---
+++ conversion.py
@@ -0,0 +1,8 @@
+import pandas as pd
+dates=[2,3,5,7,11]
+serie =pd.Series(dates)#the dt 
+print(serie)
+print(type(serie))#print in the screen the type 
+lista=serie.tolist()#using tolist aplicate the conversion 
+print(lista)
+

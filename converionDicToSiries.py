9162d2a2618d035a6b01103635a33b1865698f74
Felipe Narvaez <felipe4k@outlook.es>
2022-06-09 22:11:10 -0500

    converting a numpy array object to a series

---
+++ converionDicToSiries.py
@@ -0,0 +1,5 @@
+import pandas as pd
+#converion a set to a object series
+dates = {'a':10, 'b':20, 'c':30, 'd':40, 'e':50} #dates
+serie=pd.Series(dates)#the converion line set to serie 
+print(serie)

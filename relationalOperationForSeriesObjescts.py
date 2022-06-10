aced6e5472e46aa0856a103826a2c74f1abc1b58
Felipe Narvaez <felipe4k@outlook.es>
2022-06-09 21:17:58 -0500

    relationasl operators with pandas in series < <= > >=

---
+++ relationalOperationForSeriesObjescts.py
@@ -0,0 +1,5 @@
+from turtle import pd
+import pandas as pd
+pd1=pd.Series([1,2,3,4,5])#define the series type int 64
+pd2=pd.Series([5,4,3,2,1])
+print(pd1<=pd2)#print in the screen the operation number by number relationals or logics 
